0d3878679ab26d25829350a34580ae4b200495d1
Luiz Felipe <feliperodrigues.ime@gmail.com>
2020-04-18 13:52:04 -0300

    Projeto xadrez: tabuleiro iniciado

---
+++ xadrez-console/xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,19 @@
+﻿
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            this.qteMovimentos = 0;
+            this.tab = tab;
+        }
+    }
+}
