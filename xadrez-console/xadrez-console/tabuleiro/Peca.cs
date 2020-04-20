ee1af004b36a0785b45f6ba65745f4a881dbc634
Luiz Felipe <feliperodrigues.ime@gmail.com>
2020-04-20 18:10:36 -0300

    Projeto xadrez: mudando o nome do metodo MoverPara para MovimentoPossivel

--- xadrez-console/xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/xadrez-console/tabuleiro/Peca.cs
@@ -44,7 +44,7 @@ namespace tabuleiro
             return false;
         }
 
-        public bool PodeMoverPara(Posicao posicao)
+        public bool MovimentoPossivel(Posicao posicao)
         {
             return MovimentosPossiveis()[posicao.Linha, posicao.Coluna];
         }
--- xadrez-console/xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -111,7 +111,7 @@ namespace xadrez
 
         public void ValidarPosicaoDeDestino(Posicao origem, Posicao destino)
         {
-            if (!tabuleiro.Peca(origem).PodeMoverPara(destino))
+            if (!tabuleiro.Peca(origem).MovimentoPossivel(destino))
             {
                 throw new TabuleiroException("Posição de destino inválida.");
             }
