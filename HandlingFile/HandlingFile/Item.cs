c8a46484825b0b729c8bcdf2bbc43c9e3728f1dd
Luiz Felipe <feliperodrigues.ime@gmail.com>
2020-04-21 11:52:12 -0300

    Hadling files

---
+++ HandlingFile/HandlingFile/Item.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace HandlingFile
+{
+    class Item
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Item() { }
+
+        public Item(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double TotalValue()
+        {
+            return Price * Quantity;
+        }
+
+        public override string ToString()
+        {
+            return Name + "," + TotalValue().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ HandlingFile/HandlingFile/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace HandlingFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Felipe\Desktop\study\C# Udemy\11 Trabalhando com arquivos";
+
+            try
+            {
+                List<Item> items = new List<Item>();
+                Item item = new Item();
+
+                using (FileStream fs = new FileStream(path + @"/source.csv", FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string[] line = sr.ReadLine().Split(",");
+                            item = new Item(line[0], double.Parse(line[1])/100, int.Parse(line[2]));
+                            items.Add(item);
+                        }
+                    }
+                }
+
+                using (StreamWriter sw = File.AppendText(path + @"\out\summary.csv"))
+                {
+                    foreach (Item i in items)
+                    {
+                        sw.WriteLine(i.ToString());
+                    }
+                }
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
